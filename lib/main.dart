4f1dc4b4a6035ea4b9e7c0bd212d2344771a00ee
praty <pratyushpriyam49@gmail.com>
2023-01-17 20:48:31 +0530

    On boarding screens done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:blood_bank_app/screens/on_boarding_screen1.dart';
+import 'package:blood_bank_app/screens/on_boarding_screen2.dart';
+import 'package:blood_bank_app/screens/on_boarding_screen3.dart';
 import 'package:blood_bank_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: onBoardingScreen3(),
     );
   }
 }
